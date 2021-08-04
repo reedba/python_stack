591db626a9476cdb0fbaf7d3e04d52fe4f5a57f3
reedba <rdsfence@gmail.com>
2021-08-03 20:52:20 -0400

    First initial Python Repo

---
+++ _python/python_fundamentals/hello_world/hello_world.py
@@ -0,0 +1,33 @@
+print("Hello World!")
+
+x = "Hello Python"
+print(x)
+y = 42
+print(y)
+
+name = "Brandon Reed"
+
+print("Hello", name)
+
+print("Hello "+ name)
+
+print("Hello!",777)
+
+num = 777
+
+print(f"Hello! {num}")
+
+statement = "Hello! 777"
+
+print(statement)
+
+food1 = "pizza"
+food2 = "ice cream"
+
+print("I love to eat %s and %s" % (food1,food2))
+
+print(f"I love to eat {food1} and {food2}")
+
+print(str.upper(food1))
+
+print(str.capitalize(food2))
