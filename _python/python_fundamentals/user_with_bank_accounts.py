d8ec0ec32d1fe3fe6e9bb2f25d1fbe93c2b2a407
reedba <rdsfence@gmail.com>
2021-08-26 14:59:35 -0400

    completed the Users with bank accounts without, generating random account

--- _python/python_fundamentals/user_with_bank_accounts.py
+++ _python/python_fundamentals/user_with_bank_accounts.py
@@ -1,7 +1,6 @@
 class BankAccount:
 	
-    def __init__(self, int_rate, account): 
-        self.account = account
+    def __init__(self, int_rate): 
         self.int_rate = int_rate
         self.interest = 0
         self.balance = 0
@@ -23,22 +22,34 @@ class BankAccount:
         else:
             self.balance -= 5.00
 
-Brandon = BankAccount(.003,1)
 
-Brandon.account
-Brandon.deposit(500).withdraw(200).deposit(500).yield_interest()
-print(Brandon.balance)
 
 class User:
 
-    def __init__(self, Name):
-        self.Name = Name
-        self.interest = 0
-        self.balance = 0
+    def __init__(self, name, gender, city, state, interest_rate):
+        self.name = name
+        self.gender = gender
+        self.city = city
+        self.state = state
+        self.myAccount = BankAccount(interest_rate)
 
-    def make_deposit(self, bankaccount = BankAccount("default","default")):
-        self.balance += BankAccount.balance
+    def user_deposit(self, amount):
+        self.myAccount.deposit(amount)
+        return self
+
+    def user_withdrawal(self, amount):
+        self.myAccount.withdraw(amount)
+
+    def display(self):
+        #return BankAccount.balance
+        return f"My name is: {self.name}, and I have {self.myAccount.balance} in my account"
+
+    
 
 
 
-Checking = User("Brandon")
+Brandon_Reed = User("Brandon", "Male", "Summerville", "SC", .003)
+Brandon_Reed.user_deposit(5000).user_withdrawal(2000)
+
+
+print(Brandon_Reed.display())
