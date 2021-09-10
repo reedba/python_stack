731d25da1cac63f5a4272124f47aee2aa1a66f20
reedba <rdsfence@gmail.com>
2021-09-09 20:48:10 -0400

    corrected first django assignment

--- django/django_intro/StarWarsCharacters/starWarsApp1/urls.py
+++ django/django_intro/StarWarsCharacters/starWarsApp1/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index),
-    path('new',views.new),
-    path('create', views.create),
-    path('show/<int:my_val>', views.show),
-    path('<int:my_val>/edit', views.edit),
-    path('<int:my_val>/delete', views.destroy)
-    
+   path('', views.index),
+   path('create', views.create),
+   path('<int:number>', views.show),
+   path('<int:number>/edit', views.edit),
+   path('<int:number>/delete',views.destroy) 
 ]
--- django/django_intro/StarWarsCharacters/starWarsApp1/views.py
+++ django/django_intro/StarWarsCharacters/starWarsApp1/views.py
@@ -1,31 +1,19 @@
-from django.shortcuts import render, HttpResponse 
+from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    print('Hello World')
-    return HttpResponse('placeholder to display a list of all blogs')
-
-
-def new(request):
-    print('new request')
-    return HttpResponse('placeholder to display a new form to create a new blog')
+    return HttpResponse("placeholder to later display list of blogs")
 
 def create(request):
-    print('created a page')
-    return HttpResponse('placeholder to display a creating a blog')
+    return redirect('/')
 
+def show(request, number):
+    return HttpResponse(f'placeholder to display blog number: {number}.')
 
+def edit(request, number):
+    return HttpResponse(f'placeholder to edit blog number:{number}')
 
-def show(request, my_val):
-    print(my_val)
-    return HttpResponse(f'placeholder to display blog {my_val}')
-
-def edit(request, my_val):
-    print(my_val)
-    return HttpResponse(f'placeholder to edit blog {my_val}')
-
-def destroy(request, my_val):
-    print('destroyed')
-    return HttpResponse(f'destroyed blog {my_val}')
+def destroy(request, number):
+    return redirect('/')
 
 
 # Create your views here.
