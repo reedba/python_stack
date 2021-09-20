2e1e95d8ff09a9afd2c08ad9ff106009f0161210
reedba <rdsfence@gmail.com>
2021-09-20 19:51:34 -0400

    Figuring the post method and redirect

--- django/django_intro/dojo_survey/dojo_survey_app/views.py
+++ django/django_intro/dojo_survey/dojo_survey_app/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django import http
+from django.shortcuts import render, redirect, HttpResponse
 
+
+#def index(request):
+#    HttpResponse print('This is a frist page request')
+#    return render(request, "index.html")
+
+def results(request):
+    if request.method == "GET":
+        print('This is a get response')
+        return render(request, 'results.html')
+    if request.method == "Post":
+        print('this is a post method')
+        request.session['name'] = request.POST['name']
+        request.session['location'] = request.POST['location']
+        request.session['language'] = request.POST['language']
+        request.session['comment'] = request.POST['comment']
+
+        return redirect('/results')
 
 def index(request):
-    return render(request, "index.html")
+    print('got here from redirect!')
+    return render(request, 'index.html')
 
 # Create your views here.
