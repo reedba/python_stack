2854ec12331ab892dba54649cfbbaee3b0fba95c
reedba <rdsfence@gmail.com>
2021-09-27 14:16:37 -0400

    completed and corrected random word generator

--- django/django_intro/word_generator/word_generator_app/urls.py
+++ django/django_intro/word_generator/word_generator_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('', views.random_word),
+    path('random_word',views.random_word),
+    path('reset',views.reset)
 ]
