d91c3c6f8b43afd838e383c8287892df50cfa877
reedba <rdsfence@gmail.com>
2021-10-14 11:44:13 -0400

    second attempt at dojos and ninjas

--- django/django_orm/dojos_ninjas/dojos_ninja_app/models.py
+++ django/django_orm/dojos_ninjas/dojos_ninja_app/models.py
@@ -6,15 +6,16 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
-    ninjas = []
+
+    
     
     def __repr__(self):
         return f"Dojo: {self.name} Ninjas:{self.ninjas}"
 
 
 class Ninja(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255,default=0)
+    last_name = models.CharField(max_length=255,default=0)
     dojo = models.ForeignKey(Dojo, related_name='ninjas',on_delete=models.CASCADE)
 
 
--- django/django_orm/dojos_ninjas/dojos_ninja_app/views.py
+++ django/django_orm/dojos_ninjas/dojos_ninja_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import Dojo, Ninja
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    context = {
+        'dojos':Dojo.objects.all(),
+        'ninja':Ninja.objects.all()
+    }
+    return render(request,'index.html',context)
+
+def add_dojo(request):
+    if request.method == 'POST':
+        Dojo.objects.create(name=request.POST['name'],city=request.POST['city'],state=request.POST['state'])
+    return redirect('/')
+
+def add_ninja(request):
+    if request.method == 'POST':
+        Ninja.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],dojo=request.POST['dojo'])
+    return redirect('/')
+    
