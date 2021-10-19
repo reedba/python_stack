fe85aaa0f1ecd3da4b86886540a8254b22eddafa
reedba <rdsfence@gmail.com>
2021-10-19 11:59:38 -0400

    Completed adding all template pages

--- django/django_orm/books_authors/books_authors_app/urls.py
+++ django/django_orm/books_authors/books_authors_app/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('add_book', views.add_book),
+    path('view_book/<int:book_id>',views.view_book),
+    path('book_details/<int:book_id>',views.book_details),
+    path('author_list',views.author_list),
+    path('add_author', views.add_author),
+    path('view_author/<int:author_id>',views.view_author),
+    
 ]
 
 
--- django/django_orm/books_authors/books_authors_app/views.py
+++ django/django_orm/books_authors/books_authors_app/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Book, Author
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This page works')
+    context = {
+        'books':Book.objects.all(),
+        'authors':Author.objects.all()
+
+    }
+    return render(request,'index.html',context)
+
+def add_book(request):
+    if request.method == 'POST':
+        Book.objects.create(title=request.POST['title'],description=request.POST['description'])
+    return redirect('/')
+
+def view_book(request,book_id):
+    context = {
+        'book':Book.objects.get(id = book_id),
+        'books':Book.objects.all(),
+        'authors':Author.objects.all()
+    }
+    return render(request,'book_details.html',context)
+
+def book_details(request):
+    Book.objects.create(title=request.POST['title'],description=request.POST['description'])
+    return redirect('book_details.html')
+
+
+def author_list(request):
+    context = {
+        'authors':Author.objects.all(),
+    }
+    return render(request, 'add_author.html',context)
+
+def add_author(request):
+    if request.method == 'POST':
+        Author.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], notes=request.POST['notes'])
+    return redirect('add_author.html')
+
+def view_author(request, author_id):
+    context = {
+        'author':Author.objects.get(id = author_id),
+        'books':Book.objects.all(),
+        'authors':Author.objects.all()
+    }
+    return render(request,'author_details.html',context)
+
+
+
