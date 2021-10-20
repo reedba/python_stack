2ff0137938da212a6cc1a26c8c1bd0c3ce315e24
reedba <rdsfence@gmail.com>
2021-10-20 09:56:17 -0400

    completed the Books and Authors assignment

--- django/django_orm/books_authors/books_authors_app/urls.py
+++ django/django_orm/books_authors/books_authors_app/urls.py
@@ -3,12 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('add_book', views.add_book),
+    path('create_book', views.create_book),
     path('view_book/<int:book_id>',views.view_book),
     path('book_details/<int:book_id>',views.book_details),
     path('author_list',views.author_list),
-    path('add_author', views.add_author),
+    path('create_author', views.create_author),
     path('view_author/<int:author_id>',views.view_author),
+    path('delete_author/<int:author_id>',views.delete_author),
+    path('delete_book/<int:book_id>',views.delete_book),
+    path('add_author/<int:book_id>',views.add_author),
+    path('add_book/<int:author_id>',views.add_author)
+
     
 ]
 
--- django/django_orm/books_authors/books_authors_app/views.py
+++ django/django_orm/books_authors/books_authors_app/views.py
@@ -10,7 +10,7 @@ def index(request):
     }
     return render(request,'index.html',context)
 
-def add_book(request):
+def create_book(request):
     if request.method == 'POST':
         Book.objects.create(title=request.POST['title'],description=request.POST['description'])
     return redirect('/')
@@ -20,24 +20,27 @@ def view_book(request,book_id):
         'book':Book.objects.get(id = book_id),
         'books':Book.objects.all(),
         'authors':Author.objects.all()
+        
+
     }
     return render(request,'book_details.html',context)
 
 def book_details(request):
-    Book.objects.create(title=request.POST['title'],description=request.POST['description'])
-    return redirect('book_details.html')
+    if request.method == 'POST':
+        Book.objects.create(title=request.POST['title'],description=request.POST['description'])
+    return redirect('view_book')
 
 
 def author_list(request):
     context = {
-        'authors':Author.objects.all(),
+        'authors':Author.objects.all()
     }
     return render(request, 'add_author.html',context)
 
-def add_author(request):
+def create_author(request):
     if request.method == 'POST':
         Author.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], notes=request.POST['notes'])
-    return redirect('add_author.html')
+    return redirect('/author_list')
 
 def view_author(request, author_id):
     context = {
@@ -45,7 +48,31 @@ def view_author(request, author_id):
         'books':Book.objects.all(),
         'authors':Author.objects.all()
     }
+    print(context['author'].id)
     return render(request,'author_details.html',context)
+
+def delete_author(request, author_id):
+    author = Author.objects.get(id = author_id)
+    author.delete()
+    return redirect('/author_list')
+
+def delete_book(request, book_id):
+    book = Book.objects.get(id = book_id)
+    book.delete()
+    return redirect('/')
+
+def add_author(request,book_id):
+    this_author = Author.objects.get(id = request.POST['author_selected'])
+    this_book = Book.objects.get(id = book_id)
+    this_book.authors.add(this_author)
+    return redirect(f'/view_book/{book_id}')
+
+
+def add_book(request,author_id):
+    this_author = Author.objects.get(id = author_id)
+    this_book = Book.objects.get(id = request.POST['author_selected'])
+    this_author.books.add(this_book)
+    return redirect(f'/view_book/{author_id}')
 
 
 
