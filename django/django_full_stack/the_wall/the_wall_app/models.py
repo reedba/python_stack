5a714190d3c0a86d9000fdc1cd6d9bccd7e973c7
reedba <rdsfence@gmail.com>
2021-11-03 14:58:07 -0400

    start of The Wall Assignment

---
+++ django/django_full_stack/the_wall/the_wall_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+class Message(models.Model):
+    message = models.TextField()
+    user = models.ForeignKey(User, related_name="messages", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    user_comment = models.ForeignKey(User, related_name="users_comments", on_delete = models.CASCADE)
+    message = models.ForeignKey(Message, related_name="comments", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
