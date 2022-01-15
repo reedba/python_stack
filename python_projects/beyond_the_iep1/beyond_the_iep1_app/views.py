b3a87649ef0b65dda28256aa866dfe6c70324234
Brandon Reed <rdsfence@gmail.com>
2022-01-14 21:34:39 -0500

    Added a contact button to beyond the IEP

--- python_projects/beyond_the_iep1/beyond_the_iep1_app/views.py
+++ python_projects/beyond_the_iep1/beyond_the_iep1_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def teacher_form(request):
+    return render(request, 'teacher_form.html')
