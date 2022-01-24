6f0020bee7244fa31d5098c0a3d6ef5f85b71f7e
Brandon Reed <rdsfence@gmail.com>
2022-01-24 08:16:14 -0500

    add custom media files

--- python_projects/ecommerce_2/ecommerce_2/settings.py
+++ python_projects/ecommerce_2/ecommerce_2/settings.py
@@ -125,6 +125,11 @@ STATICFILES_DIRS = [
     'ecommerce_2/static',
 ]
 
+#media files config
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
