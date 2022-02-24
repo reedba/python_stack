07fe0779f895ebf36d1f44ed146b96f7db781866
Brandon Reed <rdsfence@gmail.com>
2022-02-24 15:05:04 -0500

    completed counter

---
+++ python_flask/fundamentals/fundamentals/counter/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+
+app.secret_key = "It was me!"
+
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"] = 0
+    session['count']+=1
+    return render_template("index.html")
+
+@app.route('/reset')
+def increment():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
